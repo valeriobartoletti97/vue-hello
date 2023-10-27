21186d1f28abaca88a5a17e601526a3184afc3c2
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-10-27 15:55:03 +0200

    bonus with img completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const { createApp } = Vue
 const app = createApp({
   data() {
     return {
-        title: 'Ciao Marco e Stefano, come state?'
+        title: 'Ciao Marco e Stefano, come state?',
+        subtitle: 'Questi siete voi ogni pomeriggio:',
+        img: 'img/marco-e-stefano.png',
+        subColor: 'sub-color',
+        /* appBg: 'app-bg' */
     }
   } 
 });
